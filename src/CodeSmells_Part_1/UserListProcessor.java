edf653069f8d91126531fa6b1d7b699257ada881
user <katya.rotar02@gmail.com>
2024-04-30 17:59:44 +0300

    Part 1. Code smells. Refactoring

--- src/CodeSmells_Part_1/UserListProcessor.java
+++ src/CodeSmells_Part_1/UserListProcessor.java
@@ -2,39 +2,48 @@ package CodeSmells_Part_1;
 import java.util.ArrayList;
 
 public class UserListProcessor {
-    private ArrayList<String> usernames;
+    private final ArrayList<User> users;
 
-    public UserListProcessor(ArrayList<String> usernames) {
-        this.usernames = usernames;
+    public UserListProcessor(ArrayList<User> user) {
+        this.users = user;
     }
     public void processUserList() {
-        // Long method
-        for (int i = 0; i < usernames.size(); i++) {
-            String username = usernames.get(i);
-            System.out.println("Processing user: " + username);
-            // Some processing logic...
-            if (username.contains("admin")) {
-                System.out.println("Admin user detected!");
-            }
+        for (User username : users) {
+            processUser(username.getUserName());
         }
+    }
+    private void processUser(String username) {
+        System.out.println("Processing user: " + username);
+        if (isUserAdmin(username)) {
+            System.out.println("Admin user detected!");
+        }
+    }
+    private boolean isUserAdmin(String username) {
+        return username.contains("admin");
     }
     public int countAdminUsers() {
-        // Primitive obsession
-        int adminCount = 0;
-        for (String username : usernames) {
-            if (username.contains("admin")) {
-                adminCount++;
+        AdminCount adminCount = new AdminCount();
+        for (User username : users) {
+            if (isUserAdmin(username.getUserName())) {
+                adminCount.increment();
             }
         }
-        return adminCount;
+        return adminCount.getCount();
     }
-    // Long parameter list
-    public void addUser(String username, String email, String password, String
-            address) {
-        // Adding user...
+
+    public void addUser(User user) {
+        users.add(user);
     }
-    // Data clumps
-    public void sendEmailToUser(String username, String email) {
-        // Sending email...
+
+    public void sendEmailToUser(UserContactInfo userContactInfo) {
+        String targetUsername = userContactInfo.getUsername();
+        for (User user : users) {
+            if (user.getUserName().equals(targetUsername)) {
+                String targetEmail = userContactInfo.getEmail();
+                System.out.println("Email sent to " + targetEmail);
+                return;
+            }
+        }
+        System.out.println("User with username " + targetUsername + " not found.");
     }
 }
